4fdff79719af8d024f176bbdc03a57516e932602
ipknHama <ipknhama@gmail.com>
2014-04-01 23:02:03 +0900

    test 404

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 import urllib
 assert "Hello World!" ==  urllib.urlopen('http://localhost:8080').read()
 assert "About Flask example." ==  urllib.urlopen('http://localhost:8080/about').read()
+assert 404 == urllib.urlopen('http://localhost:8080/list').getcode()
