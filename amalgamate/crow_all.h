6e4e66ad1233257f6413e643cfb376d2cf298065
Jamie Bullock <jamie@jamiebullock.com>
2017-09-04 16:08:02 +0100

    Add support for HTTP 422 status code
    See: https://tools.ietf.org/html/rfc4918#section-11.2

--- amalgamate/crow_all.h
+++ amalgamate/crow_all.h
@@ -8893,6 +8893,7 @@ namespace crow
                 {401, "HTTP/1.1 401 Unauthorized\r\n"},
                 {403, "HTTP/1.1 403 Forbidden\r\n"},
                 {404, "HTTP/1.1 404 Not Found\r\n"},
+                {420, "HTTP/1.1 404 Unprocessable Entity\r\n"},
 
                 {500, "HTTP/1.1 500 Internal Server Error\r\n"},
                 {501, "HTTP/1.1 501 Not Implemented\r\n"},
