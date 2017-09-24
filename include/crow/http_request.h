e94520bf7fa5081ad01faaf144a1555d359527c2
abhi <abhisharma.b@gmail.com>
2017-09-23 23:30:25 -0700

    removing crow/json from response; un-necessary

--- include/crow/http_request.h
+++ include/crow/http_request.h
@@ -8,62 +8,62 @@
 
 namespace crow
 {
-    template <typename T>
-    inline const std::string& get_header_value(const T& headers, const std::string& key)
+template <typename T>
+inline const std::string& get_header_value(const T& headers, const std::string& key)
+{
+    if (headers.count(key))
     {
-        if (headers.count(key))
-        {
-            return headers.find(key)->second;
-        }
-        static std::string empty;
-        return empty;
+        return headers.find(key)->second;
+    }
+    static std::string empty;
+    return empty;
+}
+
+struct DetachHelper;
+
+struct request
+{
+    HTTPMethod method;
+    std::string raw_url;
+    std::string url;
+    query_string url_params;
+    ci_map headers;
+    std::string body;
+
+    void* middleware_context{};
+    boost::asio::io_service* io_service{};
+
+    request()
+        : method(HTTPMethod::Get)
+    {
     }
 
-	struct DetachHelper;
-
-    struct request
+    request(HTTPMethod method, std::string raw_url, std::string url, query_string url_params, ci_map headers, std::string body)
+        : method(method), raw_url(std::move(raw_url)), url(std::move(url)), url_params(std::move(url_params)), headers(std::move(headers)), body(std::move(body))
     {
-        HTTPMethod method;
-        std::string raw_url;
-        std::string url;
-        query_string url_params;
-        ci_map headers;
-        std::string body;
+    }
 
-        void* middleware_context{};
-        boost::asio::io_service* io_service{};
+    void add_header(std::string key, std::string value)
+    {
+        headers.emplace(std::move(key), std::move(value));
+    }
 
-        request()
-            : method(HTTPMethod::Get)
-        {
-        }
+    const std::string& get_header_value(const std::string& key) const
+    {
+        return crow::get_header_value(headers, key);
+    }
 
-        request(HTTPMethod method, std::string raw_url, std::string url, query_string url_params, ci_map headers, std::string body)
-            : method(method), raw_url(std::move(raw_url)), url(std::move(url)), url_params(std::move(url_params)), headers(std::move(headers)), body(std::move(body))
-        {
-        }
+    template<typename CompletionHandler>
+    void post(CompletionHandler handler)
+    {
+        io_service->post(handler);
+    }
 
-        void add_header(std::string key, std::string value)
-        {
-            headers.emplace(std::move(key), std::move(value));
-        }
+    template<typename CompletionHandler>
+    void dispatch(CompletionHandler handler)
+    {
+        io_service->dispatch(handler);
+    }
 
-        const std::string& get_header_value(const std::string& key) const
-        {
-            return crow::get_header_value(headers, key);
-        }
-
-        template<typename CompletionHandler>
-        void post(CompletionHandler handler)
-        {
-            io_service->post(handler);
-        }
-
-        template<typename CompletionHandler>
-        void dispatch(CompletionHandler handler)
-        {
-            io_service->dispatch(handler);
-        }
-
-    };
+};
 }
