e6f22fce6a5a5f99e18e78680278360c747c30ad
Igor Mróz <mrozigor@wp.pl>
2018-08-30 17:51:54 +0200

    #315 Checking if server is nullptr.

--- include/crow/app.h
+++ include/crow/app.h
@@ -129,12 +129,16 @@ namespace crow
 #ifdef CROW_ENABLE_SSL
             if (use_ssl_)
             {
-                ssl_server_->stop();
+		if (ssl_server_) {
+                    ssl_server_->stop();
+		}
             }
             else
 #endif
             {
-                server_->stop();
+		if (server_) {
+                    server_->stop();
+		}
             }
         }
 
