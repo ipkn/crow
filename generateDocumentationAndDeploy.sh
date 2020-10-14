58bc7178bbfdc1787e1af9587e27ae6a7e1514b5
Igor Mróz <mrozigor@wp.pl>
2020-10-14 21:35:49 +0200

    #7 Don't delete index.html when generating docs.

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -59,7 +59,9 @@ git config user.email "travis@travis-ci.org"
 # stayed the same and will only update the changed files. So the gh-pages branch
 # can be safely cleaned, and it is sure that everything pushed later is the new
 # documentation.
+cp index.html ..
 rm -rf *
+mv ../index.html .
 
 # Need to create a .nojekyll file to allow filenames starting with an underscore
 # to be seen on the gh-pages site. Therefore creating an empty .nojekyll file.
