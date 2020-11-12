16cae0af4a5e66dc49e29260094ad85eac497759
Eero Pajarre <Eero.Pajarre@eepsoft.fi>
2020-11-12 15:27:26 +0200

    Activate static_file support on WIN32
    static_file support seems to work, at least with
    Microsoft Visual Studio 2017 and 2109 / C++17.
    Without it the compilation fails, so activating it.

--- include/crow/http_response.h
+++ include/crow/http_response.h
@@ -134,7 +134,7 @@ namespace crow
   * middlware must call res.set_static_file_info(filename)
   * you must add route starting with /your/restricted/path/<string>
   */
-#if !defined(_WIN32)
+
         struct static_file_info{
             std::string path = "";
             struct stat statbuf;
@@ -187,7 +187,7 @@ namespace crow
                 write_streamed_string(body, adaptor);
             }
         }
-#endif
+
 /* static file support end */
         private:
             bool completed_{};
