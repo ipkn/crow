3d202d291972c0d46d7c0eded23425e8e46a1949
The-EDev <farook@the-e-dev.com>
2020-11-26 04:08:24 +0300

    allow returning all json data types, not just json objects

--- include/crow/http_connection.h
+++ include/crow/http_connection.h
@@ -412,7 +412,7 @@ namespace crow
             buffers_.clear();
             buffers_.reserve(4*(res.headers.size()+5)+3);
 
-            if (res.body.empty() && res.json_value.t() == json::type::Object)
+            if (res.body.empty() && res.json_value.t() != json::type::Null)
             {
                 res.body = json::dump(res.json_value);
             }
